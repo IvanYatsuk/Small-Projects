c463ca1290f6feba7611a8f6bc05495ae4bbabc8
Ivan Yatsuk <ivankodpua@gmail.com>
2024-05-05 23:02:03 +0300

    Fixed Stack Overfloy

--- converter.c
+++ converter.c
@@ -3,21 +3,35 @@
 int main() {
     double celsius, fahrenheit;
     int choice;
+    int result;
 
     printf("Выберите режим конвертации:\n");
     printf("1: Цельсий -> Фаренгейт\n");
     printf("2: Фаренгейт -> Цельсий\n");
     printf("Введите ваш выбор (1 или 2): ");
-    scanf("%d", &choice);
+    result = scanf("%d", &choice);
+
+    if (result != 1) {
+        printf("Ошибка ввода.\n");
+        return 1; // Возвращает ненулевой код ошибки
+    }
 
     if (choice == 1) {
         printf("Введите температуру в градусах Цельсия: ");
-        scanf("%lf", &celsius);
+        result = scanf("%lf", &celsius);
+        if (result != 1) {
+            printf("Ошибка ввода.\n");
+            return 1;
+        }
         fahrenheit = (celsius * 9 / 5) + 32;
         printf("%.2lf°C равно %.2lf°F\n", celsius, fahrenheit);
     } else if (choice == 2) {
         printf("Введите температуру в градусах Фаренгейта: ");
-        scanf("%lf", &fahrenheit);
+        result = scanf("%lf", &fahrenheit);
+        if (result != 1) {
+            printf("Ошибка ввода.\n");
+            return 1;
+        }
         celsius = (fahrenheit - 32) * 5 / 9;
         printf("%.2lf°F равно %.2lf°C\n", fahrenheit, celsius);
     } else {

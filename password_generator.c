5aec646ffc59526721b961be3182e15113459dde
IvanYatsuk <85255504+IvanYatsuk@users.noreply.github.com>
2024-05-20 23:51:29 +0300

    Update password_generator.c

--- password_generator.c
+++ password_generator.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-#include <stlib.h>
-#include <string.h>
+#include <stdlib.h>
 #include <time.h>
 int main()
 {
-    int choice = 0;
+    int choice;
     char password[16];
     char words[68] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&";
+    srand(time(NULL));
     do{
-    srand((unsigned) time(&t));
-    printf("Press 1 to start generating a password");
-    scanf("%s", &choice);
+    printf("Press 1 to start generating a password, press 0 to stop the program");
+    scanf("%d", &choice);
+    if(choice == 1){
+        
+    }else if(choice == 0){
+        printf("Stopping the Program");
+        return 0;
+    }else{
+        printf("Incorrect input");
+        break;
+    }
     return 0;
-}while(choice == 0 || choice == 1)
+}while(choice == 0 || choice == 1);
 
 }

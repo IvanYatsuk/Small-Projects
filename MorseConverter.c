67f27fdb5e992ae3e8d6ce47dbf4fb3b4b316c67
IvanYatsuk <85255504+IvanYatsuk@users.noreply.github.com>
2024-06-01 23:55:36 +0300

    Create MorseConverter.c

---
+++ MorseConverter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *dynamicArrayOfWords; // Declare a pointer to an array
+int sizeOfWords;
+char WordsInMorse;
+int main()
+{
+    printf("Enter the text in Morse language:");
+    scanf("%s", &WordsInMorse);
+    while(strlen(WordsInMorse) < 2048){
+        
+    }
+}
